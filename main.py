a97f62a93cd1dbb83ff37368f35f7c433c92dd5a
Yada <ydt.hran2@gmail.com>
2019-03-16 17:10:47 +0900

    約定 id の queue を Redis にキャッシュしておく

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ from collections import deque
 import ccxt
 import redis as redispy
 
-bf = ccxt.bitflyer()
-bf.timeout = 30000
-bf.apiKey = os.environ['BITFLYER_API_KEY']
-bf.secret = os.environ['BITFLYER_API_SECRET']
-bf.verbose = False
-
-redis = redispy.Redis(
-    connection_pool=redispy.ConnectionPool(host='localhost', port=6379, db=0))
-
 
 def set_interval(interval, expr: callable, args=(), kwargs=None):
     """
@@ -43,17 +34,27 @@ def set_interval(interval, expr: callable, args=(), kwargs=None):
 
 
 class PositionCounter:
-    def __init__(self, bf, redis):
+    CACHE_ID_COUNT = 500
+
+    def __init__(self, bf, redis, refresh_using='redis'):
         """
         :param ccxt.bitflyer bf:
         :param redispy.Redis redis:
+        :param str refresh_using: 'redis' or 'api'
         """
         self._bf = bf
         self._redis = redis
         self._position = 0.0
 
-        self._uniq_ids = deque(maxlen=300)
+        self._uniq_ids = deque(maxlen=self.CACHE_ID_COUNT)
         self._excs_queue = multiprocessing.Queue()
+
+        if refresh_using == 'redis':
+            self._refresh_using_redis()
+        elif refresh_using == 'api':
+            self._refresh_using_api()
+        else:
+            raise NotImplementedError("refresh_using は 'redis' か 'api' で指定")
 
     @property
     def position(self):
@@ -74,12 +75,25 @@ class PositionCounter:
         for exc in new_execs:
             delta = exc['amount'] if exc['side'].lower() == 'buy' else -exc['amount']
             self._position += delta
-        self._redis.set('position', round(self._position, 8))
 
-    def refresh(self):
+        # ポジション更新
+        self._redis.set('position', round(self._position, 8))
+        if new_execs:
+            # id 更新
+            self._redis.lpush('exec_ids', *[exc['id'] for exc in new_execs])
+            self._redis.ltrim('exec_ids', 0, self.CACHE_ID_COUNT)
+
+    def _refresh_using_api(self):
         self._position = self.fetch()
-        for exc in bf.fetch_my_trades('FX_BTC_JPY', limit=100):
+        for exc in self._bf.fetch_my_trades('FX_BTC_JPY', limit=100):
             self._uniq_ids.append(exc['id'])
+        self._redis.lpush('exec_ids', *self._uniq_ids)
+
+    def _refresh_using_redis(self):
+        self._position = round(float(self._redis.get('position') or 0), 8)
+
+        redis_ids = [b.decode() for b in self._redis.lrange('exec_ids', 0, -1)]
+        self._uniq_ids = deque(redis_ids, maxlen=self.CACHE_ID_COUNT)
 
     def fetch(self):
         positions = self._bf.private_get_getpositions({'product_code': 'FX_BTC_JPY'})
@@ -88,21 +102,35 @@ class PositionCounter:
         return buys - sells
 
 
-def load_executions():
-    executions = bf.fetch_my_trades('FX_BTC_JPY', limit=100)
-    for exc in executions:
-        counter.enqueue_exc(exc)
+def main():
+    def load_executions():
+        executions = bf.fetch_my_trades('FX_BTC_JPY', limit=100)
+        for exc in executions:
+            counter.enqueue_exc(exc)
+
+    bf = ccxt.bitflyer()
+    bf.timeout = 30000
+    bf.apiKey = os.environ['BITFLYER_API_KEY']
+    bf.secret = os.environ['BITFLYER_API_SECRET']
+    bf.verbose = False
+
+    redis = redispy.Redis(
+        connection_pool=redispy.ConnectionPool(host='localhost', port=6379, db=0))
+
+    counter = PositionCounter(
+        bf=bf, redis=redis, refresh_using='redis')
+
+    set_interval(10, lambda: load_executions())
+    set_interval(1, lambda: counter.update())
+    set_interval(10, lambda: print(f'bF: {counter.fetch()} BTC'))
+
+    while True:
+        time.sleep(1)
+        redis_pos = round(float(redis.get('position')), 8)
+        print(f'Redis: {redis_pos} BTC')
+        print(f'Counter: {counter.position} BTC')
+        print(f'Redis ids: {redis.lrange("exec_ids", 0, -1)}')
 
 
-counter = PositionCounter(bf=bf, redis=redis)
-counter.refresh()
-
-set_interval(10, lambda: load_executions())
-set_interval(1, lambda: counter.update())
-set_interval(10, lambda: print(f'bF: {counter.fetch()} BTC'))
-
-while True:
-    time.sleep(1)
-    redis_pos = round(float(redis.get('position')), 8)
-    print(f'Redis: {redis_pos} BTC')
-    print(f'Counter: {counter.position} BTC')
+if __name__ == '__main__':
+    main()
